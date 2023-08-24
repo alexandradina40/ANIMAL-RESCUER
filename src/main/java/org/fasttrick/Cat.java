5acbdd1c78ae243db3247e290d1263c1b0b2de36
Dina <alexandra.dina40@yahoo.com>
2023-08-24 20:58:55 +0300

    +constructors

--- src/main/java/org/fasttrick/Cat.java
+++ src/main/java/org/fasttrick/Cat.java
@@ -9,14 +9,8 @@ public class Cat extends Animal{
     String name_of_fav_activity;
     int lives;
 
-    public Cat(int a , int lh,int lhh,int ls,String f,String n,int liv)
-    {
-       age=a;
-       level_of_health=lh;
-       level_of_hungry=lhh;
-       level_of_state=ls;
-       fav_food=f;
-       name_of_fav_activity=n;
-       lives=liv;
+
+    public Cat(String n) {
+        super(n);
     }
 }
--- src/main/java/org/fasttrick/Golden_Retriver.java
+++ src/main/java/org/fasttrick/Golden_Retriver.java
@@ -9,14 +9,8 @@ public class Golden_Retriver extends Animal{
     String name_of_fav_activity;
     int level_of_happines;
 
-    public Golden_Retriver(int a , int lh,int lhh,int ls,String f,String n,int liv)
-    {
-        age=a;
-        level_of_health=lh;
-        level_of_hungry=lhh;
-        level_of_state=ls;
-        fav_food=f;
-        name_of_fav_activity=n;
-        level_of_happines=liv;
+
+    public Golden_Retriver(String n) {
+        super(n);
     }
 }
